7e6142326c3bbc6767c7eb79e622616a3654a7e0
evanlarsen <evan.larsen@gmail.com>
2016-12-11 18:45:46 -0800

    initial commit

---
+++ src/Kaggle/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kaggle.Controllers
+{
+    [Route("api/[controller]")]
+    public class StoreController : Controller
+    {
+        readonly IStore store;
+
+        public StoreController(IStore store)
+        {
+            this.store = store;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IEnumerable<Book>> Get()
+        {
+            return await store.GetBooks();
+        }
+
+        // GET api/store/id={guid}
+        [HttpGet("{id}")]
+        public async Task<Book> Get(Guid id)
+        {
+            return await store.GetBook(id);
+        }
+
+        // POST api/store
+        [HttpPost]
+        public async Task<Guid> Post([FromBody]Book book)
+        {
+            return await store.SaveBook(book);
+        }
+
+        // PUT api/store/
+        [HttpPut]
+        public async Task Put([FromBody]Book book)
+        {
+            await store.SaveBook(book);
+        }
+
+        // DELETE api/store/id={guid}
+        [HttpDelete("{id}")]
+        public async Task Delete(Guid id)
+        {
+            await store.DeleteBook(id);
+        }
+    }
+}
---
+++ src/Kaggle/Store/Store.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kaggle
+{
+    public interface IStore
+    {
+        Task<List<Book>> GetBooks();
+        Task<Guid> SaveBook(Book book);
+        Task<Book> GetBook(Guid id);
+        Task DeleteBook(Guid id);
+    }
+    public class Store : IStore
+    {
+        readonly List<Book> books;
+
+        public Store()
+        {
+            this.books = new List<Book>();
+            this.books.AddRange(new List<Book>
+            {
+                new Book { Id = Guid.NewGuid(), Name = "Peter Pan", Price = 2 },
+                new Book { Id = Guid.NewGuid(), Name = "", Price = 5 }
+            });
+        }
+
+        public Task<List<Book>> GetBooks()
+        {
+            return Task.FromResult(books);
+        }
+
+        public Task<Book> GetBook(Guid id)
+        {
+            var storedBook = books.FirstOrDefault(b => b.Id.Equals(book.Id));
+            if (storedBook == null)
+            {
+                throw new ArgumentException($"book with id {id} was not found");
+            }
+            return Task.FromResult(storedBook);
+        }
+
+        public Task DeleteBook(Guid id)
+        {
+            var book = GetBook(id).Result;
+            books.Remove(book);
+            return Task.FromResult(true);
+        }
+
+        public Task<Guid> SaveBook(Book book)
+        {
+            if (string.IsNullOrWhiteSpace(book.Name))
+            {
+                throw new ArgumentException("book name is required");
+            }
+            if (book.Id == null)
+            {
+                book.Id = Guid.NewGuid();
+            }else
+            {
+                UpdateBook(book);
+            }
+            return Task.FromResult(book.Id);
+        }
+
+        void UpdateBook(Book book)
+        {
+            var storedBook = GetBook(book.Id).Result;
+            storedBook.Name = book.Name;
+            storedBook.Price = book.Price;
+        }
+
+        void InsertBook(Book book)
+        {
+            books.Add(book);
+        }
+    }
+
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
