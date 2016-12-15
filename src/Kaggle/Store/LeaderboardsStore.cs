271b2bbfae7a6a32e60e70045af2ef388986aad5
evanlarsen <evan.larsen@gmail.com>
2016-12-14 18:28:46 -0800

    adding xunit testing

---
+++ src/Kaggle/Store/LeaderboardsStore.cs
@@ -0,0 +1,32 @@
+﻿using FileHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kaggle.Store
+{
+    public class LeaderboardsStore
+    {
+        public List<Record> Read()
+        {
+            var engine = new FileHelperEngine<Record>();
+            var records = engine.ReadFile("leaderboards.csv");
+            return records.ToList();
+        }
+    }
+
+    [DelimitedRecord(",")]
+    public class Record
+    {
+        ////CompetitionName,TeamName,UserNames,Score,ScoreFirstSubmittedDate,NumSubmissions
+        public string CompetitionName;
+        public string TeamName;
+        public string UserNames;
+        [FieldConverter(ConverterKind.Decimal, ".")]
+        public double Score;
+        [FieldConverter(ConverterKind.Date)]
+        public DateTime ScoreFirstSubmittedDate;
+        public int NumSubmissions;
+    }
+}
---
+++ src/Kaggle/Store/LeaderboardsStore.spec.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kaggle.Store
+{
+    public class LeaderboardsStoreSpec
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
